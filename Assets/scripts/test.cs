a7452a44a6aad0146d5edef3bd4738e6b2f8faef
niedongsheng <niedongsheng@doctorwork.com>
2018-04-03 19:10:56 +0800

    test

---
+++ Assets/scripts/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am log");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
